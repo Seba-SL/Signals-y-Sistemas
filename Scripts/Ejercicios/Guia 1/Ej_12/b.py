8ba47765839295dbcf4fa18b6650d790a1ad15ff
Seba-SL <slazo@fi.uba.ar>
2024-09-20 09:22:42 -0300

    guia

--- Scripts/Ejercicios/Guia 1/Ej_12/b.py
+++ Scripts/Ejercicios/Guia 1/Ej_12/b.py
@@ -5,19 +5,20 @@ def unit_step(n):
     return np.where(n >= 0, 1, 0)
 
 # Definir el rango de n
-n = np.arange(-10, 11)  # Puedes ajustar el rango según sea necesario
+k = np.arange(-2, 4)  # Puedes ajustar el rango según sea necesario
 
 # Generar la función escalón unitario
-u_n = unit_step(n)
+u_n = unit_step(k)
 
-# Generar la señal 2n * u(n)
-signal = 2 * n * u_n
+# Generar la señal 2n * u(n
+signal = (1/7)*np.exp(-1j*6*np.pi*k/7)*np.sin(8*np.pi*k/7)/ np.sin(2*np.pi*k/7) 
+
 
 # Crear la gráfica
-plt.stem(n, signal, use_line_collection=True)
-plt.xlabel('n')
-plt.ylabel('x[n]')
-plt.title('Señal 2n * u(n)')
+plt.stem(k, signal, use_line_collection=True)
+plt.xlabel('k')
+plt.ylabel('a(k)')
+plt.title('Coeficientes de Fourier')
 plt.grid(True)
 plt.show()
 
