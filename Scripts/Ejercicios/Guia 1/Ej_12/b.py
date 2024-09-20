f23fe1b41e0dab6bcee83c01ecbc47d5597b10d6
Seba-SL <slazo@fi.uba.ar>
2024-09-20 14:58:03 -0300

    Guia

--- Scripts/Ejercicios/Guia 1/Ej_12/b.py
+++ Scripts/Ejercicios/Guia 1/Ej_12/b.py
@@ -5,15 +5,16 @@ def unit_step(n):
     return np.where(n >= 0, 1, 0)
 
 # Definir el rango de n
-k = np.arange(-2, 4)  # Puedes ajustar el rango según sea necesario
+k = np.arange(-10, 10)  # Puedes ajustar el rango según sea necesario
 
 # Generar la función escalón unitario
 u_n = unit_step(k)
 
 # Generar la señal 2n * u(n
-signal = (1/7)*np.exp(-1j*6*np.pi*k/7)*np.sin(8*np.pi*k/7)/ np.sin(2*np.pi*k/7) 
+signal = (1/6)*np.exp(-1j*np.pi*k/2)*(    np.sin(2*np.pi*k/3)/ np.sin(np.pi*k/6) )
 
-
+#  exp(-j*π*k/2) * sen(2π*k/3)
+   #                      sen(π*k/6)
 # Crear la gráfica
 plt.stem(k, signal, use_line_collection=True)
 plt.xlabel('k')
--- Scripts/Ejercicios/Guia 1/Ej_12/c.py
+++ Scripts/Ejercicios/Guia 1/Ej_12/c.py
@@ -7,15 +7,16 @@ def unit_step(x):
 
 n = np.arange(-10, 10)  # Ejemplo de valores de n
 
-signal = (2.0 ** -n) * unit_step(2 - n)
+signal = (1/7)*np.exp(-3j*np.pi*n/7)*(    np.sin(4*np.pi*n/7)/ np.sin(np.pi*n/7) )
 
-
+#(1/7) *   exp(-3*j*π*k/7) * sen(4π*k/7)
+#                             sen(π*k/7)
 
 # Crear la gráfica
 plt.stem(n, signal,label = 'c)' , use_line_collection=True)
-plt.xlabel('n')
-plt.ylabel('x[n]')
-plt.title('Ej 12  item c')
+plt.xlabel('k')
+plt.ylabel('a[k]')
+plt.title('Coeficientes de Fourier')
 plt.grid(True)
 plt.show()
 
