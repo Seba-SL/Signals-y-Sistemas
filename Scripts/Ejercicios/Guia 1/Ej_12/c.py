5789c6f514f76778744ff474adc18f91c640394b
Seba-SL <slazo@fi.uba.ar>
2024-09-23 06:25:42 -0300

    Modificacion

--- Scripts/Ejercicios/Guia 1/Ej_12/c.py
+++ Scripts/Ejercicios/Guia 1/Ej_12/c.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fftpack import fft, fftfreq
 
 
 def unit_step(x):
@@ -11,17 +12,26 @@ n = np.arange(-10, 10)  # Ejemplo de valores de n
 float_value = float(-1)
 power_value = float_value**n
 entrada = (power_value)
-signal = (power_value)*(1/(4-1j*np.pi)   + 1/(4+1j*np.pi ) )
+coef = np.sin(np.pi*n/6)*(3/2)/(12*np.sin(np.pi*n/12) )
+signal =  1 + np.cos(np.pi*n) 
+
+transformada_Fourier = fft(signal)
+
+#     a[k] = sen(π*k/6)(3/2)
+#             12sen(π*k/12)
+
+#Profe: ak = (1/12) * (1 + 2 * np.cos((np.pi/6) * k))
+
 
 #(1/7) *   exp(-3*j*π*k/7) * sen(4π*k/7)
 #                             sen(π*k/7)
 
 # Crear la gráfica
-plt.stem(n, entrada,label = '' , use_line_collection=True , linefmt='g', markerfmt='g', basefmt='g')
+plt.stem(n, transformada_Fourier,label = 'añ' , use_line_collection=True , linefmt='g', markerfmt='g', basefmt='g')
 
-plt.xlabel('t')
-plt.ylabel('x(t)')
-plt.title('entrada del Sistema')
+plt.xlabel('k')
+plt.ylabel('ak')
+plt.title('Coeficientes de Fourier')
 plt.grid(True)
 plt.show()
 
